cf68bc29fffab14ef3a4b06e4ae1a7536ac80947
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-31 20:13:11 -0300

    Update navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,31 @@
 import React, { ReactElement } from 'react'
+import { getNameInitials } from '../utils/getNameInitials'
+
 import { Container } from '../styles/components/navbar'
 
 import icon from '../images/icon.png'
 
-const Navbar = (): ReactElement => (
-  <Container>
-    <nav>
-      <div className="project-name">
-        <img src={icon} alt="logo" />
-        <span>Squad Management Tool</span>
-      </div>
-      <div className="user-profile">
-        <span id="first-name">John</span>
-        <span id="last-name">Doe</span>
-        <div id="profile-image">
-          JD
+const Navbar = (): ReactElement => {
+  const firstName = 'John'
+  const lastName = 'Doe'
+
+  return (
+    <Container>
+      <nav>
+        <div className="project-name">
+          <img src={icon} alt="logo" />
+          <span>Squad Management Tool</span>
         </div>
-      </div>
-    </nav>
-  </Container>
-)
+        <div className="user-profile">
+          <span id="first-name">{firstName}</span>
+          <span id="last-name">{lastName}</span>
+          <div id="profile-image">
+            {getNameInitials(`${firstName} ${lastName}`)}
+          </div>
+        </div>
+      </nav>
+    </Container>
+  )
+}
 
 export default Navbar
--- src/styles/components/navbar.ts
+++ src/styles/components/navbar.ts
@@ -11,6 +11,8 @@ export const Container = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+
+    position: fixed;
   }
 
   nav .project-name {
