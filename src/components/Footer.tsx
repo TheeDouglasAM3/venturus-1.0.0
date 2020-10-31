4bed306322e63d2930e1306d4e5e5e8dae4cf062
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-31 20:14:28 -0300

    Create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+
+import { Container } from '../styles/components/footer'
+
+const Navbar = (): ReactElement => (
+  <Container>
+    <footer>
+      <span>2020 - All rights reserved</span>
+    </footer>
+  </Container>
+)
+
+export default Navbar
---
+++ src/styles/components/footer.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+
+  footer { 
+    grid-area: footer;
+
+    width: 100%;
+    padding: 0.8rem;
+    margin-top: 1.2rem;
+    background: #E9E9E9;
+    text-align: center;
+    font-size: 1.0rem;
+    position: absolute;
+    left: 0;
+  }
+`
