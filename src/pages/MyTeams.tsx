84f33bdae9def5cf81d317bc13b5b7ef00e34f55
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-31 20:16:09 -0300

    Create MyTeams page

---
+++ src/pages/MyTeams.tsx
@@ -0,0 +1,81 @@
+import React, { ReactElement } from 'react'
+import { Link } from 'react-router-dom'
+import { FiPlus } from 'react-icons/fi'
+import { getNameInitials } from '../utils/getNameInitials'
+
+import Navbar from '../components/Navbar'
+import TableMyTeams from '../components/TableMyTeams'
+import TopFiveList from '../components/TopFiveList'
+import Footer from '../components/Footer'
+
+import { Container } from '../styles/pages/myteams'
+
+const MyTeams = (): ReactElement => (
+  <>
+    <Navbar />
+    <Container>
+      <div id="page-my-teams">
+
+        <section id="my-teams" className="border-area">
+
+          <header>
+            <h2>My Teams</h2>
+            <Link to="/">
+              <FiPlus size={24} color="#FFF" />
+            </Link>
+          </header>
+          <hr />
+          <TableMyTeams />
+        </section>
+
+        <section id="stats">
+
+          <div id="top-five" className="border-area">
+            <header>
+              <h2>Top 5</h2>
+            </header>
+            <hr />
+            <TopFiveList />
+          </div>
+
+          <div id="player-stats">
+
+            <div className="picked-player">
+              <h3>Most picked player</h3>
+              <div className="player-area">
+
+                <div className="player-display">
+                  <span>{getNameInitials('Cristiano Ronaldo')}</span>
+                </div>
+
+                <div className="player-percentage">
+                  <span>75%</span>
+                </div>
+
+              </div>
+            </div>
+
+            <div className="picked-player">
+              <h3>Less picked player</h3>
+              <div className="player-area">
+
+                <div className="player-display">
+                  <span>{getNameInitials('Ronaldo Luiz de Alves')}</span>
+                </div>
+
+                <div className="player-percentage">
+                  <span>25%</span>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </section>
+        <Footer />
+      </div>
+
+    </Container>
+  </>
+)
+
+export default MyTeams
---
+++ src/styles/pages/myteams.ts
@@ -0,0 +1,128 @@
+import styled from 'styled-components'
+
+import bgFootball from '../../images/bgfootball.png'
+
+export const Container = styled.main`
+  #page-my-teams {
+    width: 100vw;
+    height: 100vh;
+    padding: 5.4rem 1.8rem 0rem 1.8rem; 
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 1.6rem;
+    grid-template-areas: 
+      "myteams stats"
+      "footer footer"
+    ;
+  }
+
+  #page-my-teams .border-area {
+    width: 100%;
+    background: #FFF;
+    border: solid 1px #EEE;
+    border-radius: 0.8rem;
+  }
+
+  #page-my-teams #my-teams {
+    grid-area: myteams;
+
+    min-height: 32rem;
+  }
+
+  #page-my-teams .border-area header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  #page-my-teams .border-area header h2{
+    padding: 1.8rem;
+  }
+
+  #page-my-teams .border-area header a{
+    margin: 1.2rem 1.8rem 1.2rem 0rem;
+  }
+
+  #page-my-teams .border-area header h2 {
+    color: #532d8c;
+  }
+
+  #page-my-teams #my-teams header a {
+    width: 2.8rem;
+    height: 2.8rem;
+    background: linear-gradient(180deg, #bf34b1 0%, #532d8c 100%);
+    border-radius: 0.8rem;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    transition: box-shadow 0.4s;
+  }
+
+  #page-my-teams #my-teams header a:hover {
+    box-shadow: 3px 11px 16px -10px rgba(0,0,0,0.5);
+  }
+
+  #page-my-teams #stats {
+    grid-area: stats;
+    
+    width: 100%;
+  }
+
+  #page-my-teams #stats #player-stats {
+    height: 18rem;
+    margin-top: 1.8rem;
+    background-image: url('${bgFootball}'), linear-gradient(180deg, #bf34b1 0%, #532d8c 100%);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+    border-radius: 0.8rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #page-my-teams #stats #player-stats .picked-player {
+    width: 100%;
+    padding: 1.8rem;
+  }
+
+  #page-my-teams #stats #player-stats .picked-player h3 {
+    color: #FFF;
+    margin-bottom: 1.8rem;
+    text-align: center;
+  }
+
+  #page-my-teams #stats #player-stats .picked-player .player-area {
+    display: flex;
+    justify-content: center;
+  }
+
+  #page-my-teams #stats #player-stats .picked-player .player-area .player-display {
+    width: 70%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #page-my-teams #stats #player-stats .picked-player .player-area .player-display span {
+    background: #FFF;
+    width: 8rem;
+    height: 8rem;
+    border-radius: 8rem;
+    text-align: center;
+    line-height: 8rem;
+    font-size: 3.6rem;
+  }
+
+  #page-my-teams #stats #player-stats .picked-player .player-area .player-percentage span{
+    width: 30%;
+    color: #FFF;
+    font-weight: bold;
+    font-size: 1.6rem;
+    vertical-align: top;
+    border-bottom: solid 2px #FFF;
+  }
+`
