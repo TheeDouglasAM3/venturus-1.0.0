1fb7b707b545a32720bd07bc6378648063ab6e17
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-31 20:15:10 -0300

    Create component TableMyTeams

---
+++ src/components/TableMyTeams.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement } from 'react'
+import { FiEdit, FiShare2, FiTrash } from 'react-icons/fi'
+import { Container } from '../styles/components/tablemyteams'
+
+const TableMyTeams = (): ReactElement => (
+  <Container>
+    <table>
+      <thead>
+        <tr>
+          <th id="name-column">Name</th>
+          <th id="description-column">Instrument</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>John Lennon</td>
+          <td className="description">
+            <span>Rhythm Guitar</span>
+            <div className="action-buttons">
+              <FiEdit size={16} />
+              <FiShare2 size={16} />
+              <FiTrash size={16} />
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </Container>
+)
+
+export default TableMyTeams
---
+++ src/styles/components/tablemyteams.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  table { 
+    border-spacing: 0px;
+    text-align: left;
+    width: 100%;
+    padding: 1.8rem;
+  }
+
+  table tr th:first-child,  
+  table tr td:first-child {
+      border-top-left-radius: 10px; 
+      border-bottom-left-radius: 10px;
+  }
+  
+  table tr th:last-child, 
+  table tr td:last-child {
+      border-bottom-right-radius: 10px; 
+      border-top-right-radius: 10px; 
+  }
+
+  table tr th,
+  table tr td{
+    padding: 1.2rem;
+  }
+
+  table tr:hover{
+    background: #F0F0F0;
+    color: #f2295b;
+  }
+
+  table tr #name-column {
+    width:25%
+  }
+
+  .description{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .description .action-buttons{
+    display: flex;
+    column-gap: 0.8rem;
+    cursor: pointer;
+  }
+`
