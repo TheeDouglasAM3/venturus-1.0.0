9647af82086d35de1c3f57f9e39b89922389d725
Douglas Marcelino <douglas_champion@hotmail.com>
2020-11-01 21:47:38 -0300

    Create ManageTeam page

--- src/pages/ManageTeam.tsx
+++ src/pages/ManageTeam.tsx
@@ -1,11 +1,139 @@
-import React, { ReactElement } from 'react'
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { ReactElement, useState } from 'react'
 
 import Navbar from '../components/Navbar'
+import TagInput from '../components/TagInput'
+import Footer from '../components/Footer'
 
-const ManageTeam = (): ReactElement => (
-  <>
-    <Navbar />
-  </>
-)
+import { Container } from '../styles/pages/manageteam'
+
+const ManageTeam = (): ReactElement => {
+  const [tags, setTags] = useState(['example tag'])
+
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <div id="page-manage-team">
+          <section id="create-your-team" className="border-area">
+
+            <header>
+              <h2>Create your team</h2>
+            </header>
+            <hr />
+
+            <section id="team-information">
+              <h3>TEAM INFORMATION</h3>
+              <div className="form-div">
+
+                <div className="fields-area">
+                  <span className="form-field-name">Team Name</span>
+                  <label htmlFor="name">
+                    <input type="text" id="name" name="name" placeholder="Insert team name" />
+                  </label>
+
+                  <span className="form-field-name">Description</span>
+                  <label htmlFor="description">
+                    <textarea id="description" name="description" />
+                  </label>
+                </div>
+
+                <div className="fields-area">
+                  <span className="form-field-name">Team Website</span>
+                  <label htmlFor="website">
+                    <input type="text" id="website" name="website" placeholder="http://myteam.com" />
+                  </label>
+
+                  <span className="form-field-name">Team Type</span>
+                  <div className="radio-area">
+                    <div className="radio-line">
+                      <input type="radio" id="real" name="typeTeam" value="real" />
+                      <label htmlFor="real" id="first-radio">Real</label>
+                    </div>
+                    <div className="radio-line">
+                      <input type="radio" id="fantasy" name="typeTeam" value="fantasy" checked />
+                      <label htmlFor="fantasy">Fantasy</label>
+                    </div>
+                  </div>
+                  <br />
+
+                  <span className="form-field-name">Tags</span>
+                  <TagInput />
+
+                </div>
+              </div>
+            </section>
+
+            <section id="configure-squad">
+              <h3>CONFIGURE SQUAD</h3>
+
+              <div className="form-div">
+                <div className="fields-area">
+                  <span className="form-field-name">
+                    <label htmlFor="position">Formation</label>
+                  </span>
+                  <select name="position" id="position">
+                    <option value="1">3-2-2-3</option>
+                    <option value="2">3-2-3-1</option>
+                    <option value="3">3-4-3</option>
+                    <option value="4">3-5-2</option>
+                    <option value="5">4-2-3-1</option>
+                    <option value="6">4-3-1-1</option>
+                    <option value="7">4-3-2</option>
+                    <option value="8">4-4-2</option>
+                    <option value="9">4-5-1</option>
+                    <option value="10">5-4-1</option>
+                  </select>
+
+                  <div id="position-players">
+                    <span>+ </span>
+                    <span>+ </span>
+                    <span>+ </span>
+                    <span>+ </span>
+                    <span>+ </span>
+                    <span>+ </span>
+                    <span>+ </span>
+                    <span>+ </span>
+                    <span>+ </span>
+                    <span>+ </span>
+                    <span>+ </span>
+                    <span>+ </span>
+                    <span className="hide-circle">+ </span>
+                    <span>+ </span>
+                    <span className="hide-circle">+ </span>
+                  </div>
+                </div>
+                <div className="fields-area">
+                  <span className="form-field-name">Search Players</span>
+                  <label htmlFor="player-name">
+                    <input type="text" id="player-name" name="playerName" placeholder="Insert player name" />
+                  </label>
+
+                  <div id="players-list">
+                    <div className="player-info">
+                      <div className="info-left">
+                        <strong>Name: </strong>
+                        <span>Cristiano Ronaldo</span>
+                        <br />
+                        <strong>Nacionality: </strong>
+                        <span>Portugal</span>
+                      </div>
+                      <div className="info-right">
+                        <strong>Age: </strong>
+                        <span>32</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+          </section>
+        </div>
+        <Footer />
+      </Container>
+    </>
+  )
+}
 
 export default ManageTeam
---
+++ src/styles/pages/manageteam.ts
@@ -0,0 +1,193 @@
+import styled from 'styled-components'
+
+import bgFootball from '../../images/bgfootballx.png'
+
+export const Container = styled.main`
+
+  .form-field-name {
+    margin-bottom: 0.8rem;
+  }
+
+  #page-manage-team {
+    width: 100vw;
+    height: auto;
+    padding: 5.4rem 1.8rem 0rem 1.8rem; 
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-column-gap: 1.6rem;
+    grid-template-areas: 
+      "form"
+      "footer"
+    ;
+  }
+
+  #page-manage-team .border-area {
+    width: 100%;
+    background: #FFF;
+    border: solid 1px #EEE;
+    border-radius: 0.8rem;
+  }
+
+  #page-manage-team #create-your-team {
+    grid-area: form;
+
+    min-height: 32rem;
+  }
+
+  #page-manage-team .border-area header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  #page-manage-team .border-area header h2{
+    padding: 1.8rem;
+  }
+
+  #page-manage-team .border-area header a{
+    margin: 1.2rem 1.8rem 1.2rem 0rem;
+  }
+
+  #page-manage-team .border-area header h2 {
+    color: #532d8c;
+  }
+
+  .form-div {
+    display:flex;
+    justify-content: center;
+    padding: 0rem 12rem 0rem 12rem;
+    column-gap: 6rem;
+  }
+
+  .form-div .fields-area{
+    width: 100%;
+    display:flex;
+    flex-direction:column;
+  }
+
+  .radio-area {
+    display: flex;
+    align-items: center;
+  }
+
+  .radio-line {
+    margin-right: 6rem;
+  }
+
+  #position-players {
+    width: 24rem;
+    height: 36rem;
+    margin-bottom: 1.8rem;
+    border-radius: 1rem;
+
+    background-image: url('${bgFootball}'), linear-gradient(180deg, #bf34b1 0%, #532d8c 100%);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+
+    display: grid;
+    grid-template-columns: 100px 100px 100px;
+    justify-content: center;
+    align-items: center;
+    align-self: center;
+    column-gap: 3%;
+  }
+
+  #position-players span {
+    width: 6rem;
+    height: 6rem;
+    margin: 0.1rem;
+    background: rgba(255, 255, 255, 0.6);
+    border-radius: 10rem;
+    font-size: 2rem;
+    font-weight: 700;
+    color: #FFF;
+    border: dashed 2px #FFF;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h3 {
+    color: #A9A9A9;
+    display: flex;
+    justify-content: center;
+    margin: 1.8rem 0rem 1.8rem 0rem;
+  }
+
+  textarea {
+    min-height: 12.8rem;
+  }
+
+  input[type="text"],
+  select,
+  textarea {
+    width: 100%;
+    margin-bottom: 2.8rem;
+    border-radius: 0.4rem;
+    padding: 0.6rem 0.8rem 0.6rem 0.8rem;
+    border: solid 2px #CDCDCD;
+  }
+
+  input:focus,
+  select:focus,
+  textarea:focus {
+    border: solid 2px #f2295b;
+    outline: none;
+    box-shadow: none;
+  }
+
+  input[type="text"] {
+    min-height: 1.2rem;
+  }
+
+  input:checked ~ label{
+    color: #f2295b;
+  }
+
+  select {
+    font-size: 1.1rem;
+  }
+
+  input[type='radio'] {
+    -webkit-appearance:none;
+    width:14px;
+    height:14px;
+    border:1px solid darkgray;
+    border-radius:50%;
+    outline:none;
+    margin-right: 0.2rem;
+  }
+
+  input[type='radio']:before {
+    content:'';
+    display:block;
+    width:100%;
+    height:100%;   
+    border-radius:50%;     
+  }
+
+  .hide-circle {
+    visibility: hidden;
+  }
+
+  input[type='radio']:checked:before {
+    background: linear-gradient(180deg, #bf34b1 0%, #532d8c 100%);
+  }
+
+  #players-list .player-info {
+    background: linear-gradient(180deg, #FAFAFA 24%, #A9A9A9 100%);
+    padding: 1rem;
+    line-height: 2rem;
+    border: dashed 2px #CDCDCD;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+  }
+
+  #players-list .player-info span {
+    color: #f2295b;
+  }
+`
