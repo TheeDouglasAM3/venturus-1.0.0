803e1ad39c0f88d4c3f5fd03d43ce61af05c3f6b
Douglas Marcelino <douglas_champion@hotmail.com>
2020-11-03 20:41:31 -0300

    Add teams of LocalStorage in the table

--- src/components/TableMyTeams.tsx
+++ src/components/TableMyTeams.tsx
@@ -1,53 +1,69 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { FiEdit, FiShare2, FiTrash } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid'
 import { Container } from '../styles/components/tablemyteams'
 
-const TableMyTeams = (): ReactElement => (
-  <Container>
-    <table>
-      <thead>
-        <tr>
-          <th id="name-column">Name</th>
-          <th id="description-column">Instrument</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>John Lennon</td>
-          <td className="description">
-            <span>Rhythm Guitar</span>
-            <div className="action-buttons">
-              <FiEdit size={16} />
-              <FiShare2 size={16} />
-              <FiTrash size={16} />
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td>John Lennon</td>
-          <td className="description">
-            <span>Rhythm Guitar</span>
-            <div className="action-buttons">
-              <FiEdit size={16} />
-              <FiShare2 size={16} />
-              <FiTrash size={16} />
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td>John Lennon</td>
-          <td className="description">
-            <span>Rhythm Guitar</span>
-            <div className="action-buttons">
-              <FiEdit size={16} />
-              <FiShare2 size={16} />
-              <FiTrash size={16} />
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </Container>
-)
+interface Player {
+  name: string,
+  nacionality: string,
+  age: number
+}
+
+interface Team {
+  id: string
+  name: string,
+  description?: string,
+  website: string,
+  teamType: string,
+  tags?: string[],
+  formation: string,
+  teamPlayers: Player[]
+}
+
+const TableMyTeams = (): ReactElement => {
+  const [teams, setTeams] = useState<Team[]>([])
+
+  useEffect(() => {
+    setTeams(JSON.parse(localStorage.getItem('teams') || '[]'))
+  }, [])
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th id="name-column">Name</th>
+            <th id="description-column">Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teams.map((team: Team) => (
+            <tr key={`${uuidv4()}`}>
+              <td>{team.name}</td>
+              <td className="description">
+                <span>
+                  {team.description}
+                </span>
+                <div className="action-buttons">
+                  <Link to={`/manage-team/${team.id}`}>
+                    <FiEdit size={16} />
+                  </Link>
+                  <>
+                    <FiShare2 size={16} />
+                  </>
+                  <>
+                    <FiTrash size={16} />
+                  </>
+                </div>
+              </td>
+            </tr>
+          ))}
+
+        </tbody>
+      </table>
+    </Container>
+  )
+}
 
 export default TableMyTeams
